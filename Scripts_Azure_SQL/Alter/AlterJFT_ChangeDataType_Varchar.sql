f7616df155ff43fea7aeed537c7a5c5d62786a3d
Fabricio Almeida da Silva Nunes <32113848+fabricioasn@users.noreply.github.com>
2021-05-23 02:54:56 -0300

    All nulls removed in transformation. Grades discretization and avg pending

---
+++ Scripts_Azure_SQL/Alter/AlterJFT_ChangeDataType_Varchar.sql
@@ -0,0 +1,34 @@
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_CN VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_CH VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_LC VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_MT VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_REDACAO VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_COMP1 VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_COMP2 VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_COMP3 VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_COMP4 VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_NOTA_COMP5 VARCHAR(50)
+
+alter table jft.Join_FactDim_InfoProva_InfoPessoa_STEM_QSE
+alter column NU_IDADE VARCHAR(50)
+
+
